0ec8bf7ce08d22cdde57e0067a2f531024a92759
morses-code <morsescott88@gmail.com>
2020-03-30 12:08:38 +0100

    dp-cli generated api project

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"github.com/kelseyhightower/envconfig"
+	"time"
+)
+
+// Config represents service configuration for dp-sessions-api
+type Config struct {
+	BindAddr                   string        `envconfig:"BIND_ADDR"`
+	GracefulShutdownTimeout    time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
+	HealthCheckInterval        time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
+	HealthCheckCriticalTimeout time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
+}
+
+var cfg *Config
+
+// Get returns the default config with any modifications through environment
+// variables
+func Get() (*Config, error) {
+	if cfg != nil {
+		return cfg, nil
+	}
+
+	cfg := &Config{
+		BindAddr:                   ":",
+		GracefulShutdownTimeout:    5 * time.Second,
+		HealthCheckInterval:        10 * time.Second,
+		HealthCheckCriticalTimeout: time.Minute,
+	}
+
+	return cfg, envconfig.Process("", cfg)
+}
